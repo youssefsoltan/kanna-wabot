8b91c69cbff21a2029084eea410a1be530c664ef
Ananda_Ganz <94738167+AnandaGanz-bot@users.noreply.github.com>
2022-02-24 09:02:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -34,8 +34,8 @@ function pickRandom(list) {
 
 
 global.DeveloperMode = 'false' //Buat Mode True False Moderator
-global.linkGC = ['https://chat.whatsapp.com/Hjr3H7l9iC8F2deP0cv6GT'] // ganti jadi group lu
-global.owner = ['62831433937633', '6283143393763' ,'6288217277973','62831433937630','6285704954639', '62895336282144', '6285712420674'] // Put your number here
+global.linkGC = ['https://chat.whatsapp.com/LcgnBtXnHbeJx6VOgQyoxw'] // ganti jadi group lu
+global.owner = ['6281215320837', '62831433937633', '6283143393763', '6288217277973', '62831433937630', '6285704954639', '62895336282144', '6285712420674'] // Put your number here
 global.mods = ['6285231651905'] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -73,12 +73,12 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = '🌙 Ƙαnnα вσт\nwa.me/6283143393763\n\n\n𝙱𝚢 𝙻𝚎𝚝𝚝𝚊 あ\n\n\nFollow IG: @ppiowy_\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n.'
+global.packname = '🌙 Loli_Botz\nwa.me/6281215320837\n\n\n𝙱𝚢 𝙻𝚎𝚝𝚝𝚊 あ\n\n\nFollow IG: @anandaganz7_\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n.'
 global.author = '۰ ⸼'
 
 // silakan di ganti klo mau
-global.botwm = `🎮 Ƙαnnα вσт`//UBAH JADI NAMAMU
-global.wm = `꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘Ƙαnnα - chαn˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷`
+global.botwm = `🎮 Loli_Botz By Ananda_ツ`//UBAH JADI NAMAMU
+global.wm = `꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘Lord_Ananda_ツ˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷`
 global.botdate = `❏ ${wktu}\n❏ ${week} ${date}`
 global.imagebot = `https://telegra.ph/file/793b6accf3758f092742a.jpg`
 global.logo = fs.readFileSync('./src/logo3.jpg')
