04f5e1717dbc1652e0059a90ba110693f6976e13
Ananda_Ganz <94738167+AnandaGanz-bot@users.noreply.github.com>
2022-02-24 09:21:59 +0700

    Update owneryve.js

--- plugins/owneryve.js
+++ plugins/owneryve.js
@@ -1,5 +1,5 @@
 let handler = function (m) {
-	this.sendContact(m.chat, '6285712420674', 'Owner NekoBotz :)', m)
+	this.sendContact(m.chat, '6281215320837', 'Owner NekoBotz :)', m)
 }
 
 handler.customPrefix = ['🍭Owner Kannabot'] 
