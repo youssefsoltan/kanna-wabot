a0275ad164630efe0c9d2a3929013b259127d169
darkdevil3610 <94959564+darkdevil3610@users.noreply.github.com>
2022-03-27 12:30:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ CFonts.say('Rpg\nWhatsApp Bot', {
   align: 'center',
   gradient: ['red', 'magenta']
 })
-CFonts.say(`'${package.name}' By @${package.author.name || package.author}\nKannabot ver 1.5.0\nRecode By LettaChn`, {
+CFonts.say(`'${package.name}' By @${package.author.name || package.author}\nbot ver 1.5.0\nRecode By Gourav`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
