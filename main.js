14766c94ee87a41fa259d0a34d9b144795189c49
darkdevil3610 <94959564+darkdevil3610@users.noreply.github.com>
2022-03-27 12:29:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ global.DATABASE = global.db // Backwards Compatibility
 
 global.conn = new WAConnection()
 conn.version =[2,2143,3]
-conn.browserDescription =['Kanna BOT', 'Firefox', '3.0']
+conn.browserDescription =['GOURAV BOT', 'Firefox', '3.0']
 let authFile = `${opts._[0] || 'session'}.data.json`
 if (fs.existsSync(authFile)) conn.loadAuthInfo(authFile)
 if (opts['trace']) conn.logger.level = 'trace'
